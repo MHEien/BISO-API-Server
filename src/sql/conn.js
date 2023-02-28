ee01aa116eab852b6278a06c362d655b7722ab2d
MHEien <markushei@hotmail.no>
2023-02-28 11:34:56 +0100

    db

---
+++ src/sql/conn.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+
+//Tilkobling til databasen.
+const db = mysql.createConnection({
+    host: 'server_sql',
+    user: 'mysql',
+    password: '273fd0f2b03fb1fa660b',
+    database: 'server'
+  });
+
+module.exports = db;
