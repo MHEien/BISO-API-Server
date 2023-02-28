bf980d487d763aaaf9ad1f79742f87e3194ad986
MHEien <markushei@hotmail.no>
2023-02-28 12:24:12 +0100

    Update conn.js

--- src/sql/conn.js
+++ src/sql/conn.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 //Tilkobling til databasen.
 const db = mysql.createConnection({
-    host: 'server_mysql',
+    host: 'localhost',
     user: 'root',
     password: '34aa1f3564657184c447',
     database: 'server'
